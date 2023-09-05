7671a4bd0aa7cd46c6ea1bb03a92bf0cb90c8ecd
chirag <chirag.saini@hotmail.com>
2023-09-05 13:29:05 -0400

    Removed duplicate results

--- src/main/java/com/chirag/ThreeSum.java
+++ src/main/java/com/chirag/ThreeSum.java
@@ -1,15 +1,17 @@
 package com.chirag;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // Solution to https://leetcode.com/problems/3sum/
 public class ThreeSum {
 
     public static void main(String[] args) {
 
+        System.out.println(ThreeSumSolution(new int[] {-1,0,1,2,-1,-4}));
+
     }
 
+    // this solution is correct but exceeds time - Need a lesser complexity solution
     public static List<List<Integer>>ThreeSumSolution (int [] nums) {
         int numOfValues = nums.length;
 
@@ -19,15 +21,27 @@ public class ThreeSum {
             for (int j = i; j < numOfValues ; j ++) {
                 for (int k = j; k < numOfValues ; k ++) {
                     if (nums[i] + nums[j] + nums[k] == 0 ) {
-                        List<Integer> innerList = new ArrayList<>();
-                        innerList.add(nums[i]);
-                        innerList.add(nums[j]);
-                        innerList.add(nums[k]);
-                        retVal.add(innerList);
+                        if (i!=j && i!= k && j != k) {
+                            List<Integer> innerList = new ArrayList<>();
+                            innerList.add(nums[i]);
+                            innerList.add(nums[j]);
+                            innerList.add(nums[k]);
+
+                            Collections.sort(innerList);
+
+                            if (!retVal.contains(innerList)) {
+                                retVal.add(innerList);
+                            }
+
+                        }
                     }
                 }
             }
         }
+        // remove duplicates?
+
+
+
         return retVal;
     }
 }
