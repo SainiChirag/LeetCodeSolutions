c599b9b1f4352dbe5d206fe209c796a9e7081c56
chirag <chirag.saini@hotmail.com>
2023-09-06 16:16:31 -0400

    Solution to LeetCode #5 https://leetcode.com/problems/longest-palindromic-substring/

--- src/main/java/com/chirag/KthLargestNumber.java
+++ src/main/java/com/chirag/KthLargestNumber.java
@@ -1,2 +1,31 @@
-package com.chirag;public class KthLargestNumber {
+package com.chirag;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+import static java.util.Collections.sort;
+
+public class KthLargestNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+// sorting is not to be used ..
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int num : nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.remove();
+            }
+        }
+
+        return heap.peek();
+
+    }
+
+    // another approach is to use quickSelect
 }
